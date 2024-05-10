16a00f6cf2519902bc06c9c053334fe1f8b6b913
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-10 16:20:33 +0200

    [FIX BONUS 1]

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,6 +2,6 @@
     <h1>Header</h1>
 
     <a href="{{ route('home') }}">Home</a>
-    <a href="{{ route('nuova-pagina') }}">Nuova Pagina</a>
+    {{-- <a href="{{ route('nuova-pagina') }}">Nuova Pagina</a> --}}
     <a href="{{ route('movies') }}">Movies</a>
 </header>
