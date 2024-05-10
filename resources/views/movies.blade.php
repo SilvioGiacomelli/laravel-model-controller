36b569f3a19bef7c45e2414ddcb4554de6ab401f
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-10 16:12:11 +0200

    [FINITO]

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,16 +2,47 @@
 
 @section('content')
     <h1>Movies</h1>
-    @foreach ($movies as $movie)
-        <div class="card" style="width: 18rem;">
-            {{-- <img class="card-img-top" src="{{ $movie->cover }}" alt="{{ $movie->title }}"> --}}
-            <div class="card-body">
-                <h5 class="card-title">{{ $movie->title }}</h5>
-                <p class="card-text">{{ $movie->nationality }}</p>
-                <p>{{ $movie->original_title }}</p>
-                <p>{{ $movie->date }}</p>
-                <p>{{ $movie->vote }}</p>
-            </div>
+    <div class="container">
+        <div class="row">
+
+            @foreach ($movies as $movie)
+                <div class="card" style="width: 18rem;">
+                    {{-- <img class="card-img-top" src="{{ $movie->cover }}" alt="{{ $movie->title }}"> --}}
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">{{ $movie->nationality }}</p>
+                        <p>{{ $movie->original_title }}</p>
+                        <p>{{ $movie->date }}</p>
+                        <p>{{ $movie->vote }}</p>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+
+    </div>
 @endsection
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+
+    .card {
+        margin: 10px;
+    }
+
+    .card-title {
+        font-size: 20px;
+    }
+
+    .card-text {
+        font-size: 16px;
+    }
+
+    .card p {
+        font-size: 14px;
+    }
+</style>
